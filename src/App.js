60d765c913c86c569d1559c8286ed8f715ec9808
Mark Dewey <mark.t.dewey@gmail.com>
2019-03-04 16:11:41 -0500

    something something v1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import HelloWorld from './components/HelloWorld'
+import WeatherSearch from './components/WeatherSearch'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return <WeatherSearch zip="33718" />
   }
 }
 
---
+++ src/components/WeatherSearch.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function WeatherSearch(props) {
+  const _zip = props.zip || '33716'
+  const [search, setSearch] = useState(_zip)
+  const [weather, setWeather] = useState({})
+
+  const fetchWeather = e => {
+    if (e) {
+      e.preventDefault()
+    }
+
+    const key = '5c418bd61b262dfeab5ee02852a70c07'
+    const _url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${key}`
+    if (search.length > 4) {
+      axios.get(_url).then(resp => {
+        console.log({ resp })
+        setWeather(resp.data)
+      })
+    }
+  }
+  useEffect(() => {
+    fetchWeather()
+  }, [search])
+  return (
+    <section>
+      searching for {search}
+      <br />
+      <form>
+        <input
+          type="search"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+      </form>
+      {weather.weather ? weather.weather[0].main : ''}
+    </section>
+  )
+}
